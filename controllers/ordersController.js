42eaa11853368b4d863d739d4e731887776e5a6a
Doven <doven.obertinca@gmail.com>
2021-04-26 17:05:36 +0200

    first commit with orders

--- controllers/ordersController.js
+++ controllers/ordersController.js
@@ -1,39 +1,52 @@
-const low = require('lowdb');
-const FileSync = require('lowdb/adapters/FileSync');
-const adapter = new FileSync('data/db.json');
-const db = low(adapter);
-
-
+const mongodb = require("mongodb");
+// get all records
 exports.getOrders = (req, res, next) => {
-    const orders = db.get('orders').value()
-    res.status(200).send(orders);
-}
+  // access db from global object   // select all records
+  req.app.locals.db
+    .collection("orders")
+    .find()
+    .toArray((err, docs) => {
+      res.json(docs);
+    });
+};
 
 exports.getOrder = (req, res, next) => {
-    const { id } = req.params;
-    const order = db.get('orders').find({ id });
-    res.status(200).send(order);
-}
+  const { id } = req.params;
+  const order = db.get("orders").find({ id });
+  res.status(200).send(order);
+};
 
 exports.deleteOrder = (req, res, next) => {
-    const { id } = req.params;
-    const order = db.get('orders').remove({ id }).write();
-    res.status(200).send(order);
-}
+  const { id } = req.params;
+  const order = db.get("orders").remove({ id }).write();
+};
 
-exports.updateOrder = (req, res, next) => {
-    const { id } = req.params;
-    const dt = req.body;
-    const order = db.get('orders').find({ id }).assign(dt).write();
-    res.status(200).send(order);
-}
+// exports.updateOrder = (req, res, next) => {
+//     const { id } = req.params;
+//     const order = req.body;
+//     const order = db.get('orders').find({ id }).assign(dt).write();
+
+// }
 
 exports.addOrder = (req, res, next) => {
     const order = req.body;
-    db.get('orders').push(order)
-        .last()
-        .assign({ id: Date.now().toString() })
-        .write();
+    //access db from globel object 
+    req.app.locals.db.collection('orders').insertOne(order,(err,entry)=>{
+    res.json(entry)
+    }
+    );
+};
 
-    res.status(200).send(order);
-}
+exports.updateOrder = (req, res, next) => {
+  const { id } = req.params;
+  const order = req.body;
+  req.app.locals.db.collection("orders").updateOne(
+    { _id: new mongodb.ObjectID(id) },
+    {
+      $set: order,
+    },
+    (err, entry) => {
+      res.json(entry);
+    }
+  );
+};
